d26ea9ce917a6bcbd9699c9e4fdac9abd0610b60
sans-connaissance <davemalicke@gmail.com>
2021-08-16 20:31:15 -0400

    Adding motion detection and control with CoreMotion

--- SpaceInvaders-2/GameScene.swift
+++ SpaceInvaders-2/GameScene.swift
@@ -7,10 +7,15 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
 class GameScene: SKScene {
     
+    
     var contentCreated = false
+    
+    
+    let motionManager = CMMotionManager()
     
     // 1
     var invaderMovementDirection: InvaderMovementDirection = .right
@@ -21,25 +26,25 @@ class GameScene: SKScene {
     
     
     enum InvaderMovementDirection {
-      case right
-      case left
-      case downThenRight
-      case downThenLeft
-      case none
+        case right
+        case left
+        case downThenRight
+        case downThenLeft
+        case none
     }
     
     enum InvaderType {
-      case a
-      case b
-      case c
-      
-      static var size: CGSize {
-        return CGSize(width: 24, height: 16)
-      }
-      
-      static var name: String {
-        return "invader"
-      }
+        case a
+        case b
+        case c
+        
+        static var size: CGSize {
+            return CGSize(width: 24, height: 16)
+        }
+        
+        static var name: String {
+            return "invader"
+        }
     }
     
     let kInvaderGridSpacing = CGSize(width: 12, height: 12)
@@ -55,15 +60,18 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-//        if !self.contentCreated {
-//          self.createContent()
-//          self.contentCreated = true
-//        }
+        // add accelerometer monitor to scene.
+        motionManager.startAccelerometerUpdates()
+        
+        //        if !self.contentCreated {
+        //          self.createContent()
+        //          self.contentCreated = true
+        //        }
         
         setupInvaders()
         setupShip()
         setupHud()
-       
+        
         
     }
     
@@ -98,202 +106,216 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
-    
+        processUserMotion(forUpdate: currentTime)
         // this is where you can make things move
         moveInvaders(forUpdate: currentTime)
     }
     
     
-//    func createContent() {
-//
-//      let invader = SKSpriteNode(imageNamed: "InvaderA_00.png")
-//
-//      invader.position = CGPoint(x: 0, y: 0)
-//
-//      self.addChild(invader)
-//
-//      // black space color
-//      self.backgroundColor = SKColor.black
-//    }
+    //    func createContent() {
+    //
+    //      let invader = SKSpriteNode(imageNamed: "InvaderA_00.png")
+    //
+    //      invader.position = CGPoint(x: 0, y: 0)
+    //
+    //      self.addChild(invader)
+    //
+    //      // black space color
+    //      self.backgroundColor = SKColor.black
+    //    }
     
     // how many different types of moving options can I create?
     // can I create some kind of engine / or set of functions that call
     //  each other in order to drive unique movements at a higher order of the code?
     // what does my movement engine look like?
     func moveInvaders(forUpdate currentTime: CFTimeInterval) {
-      // 1
-      if currentTime - timeOfLastMove < timePerMove {
-        return
-      }
-      
-      // 2
-        enumerateChildNodes(withName: InvaderType.name) { node, stop in
-        switch self.invaderMovementDirection {
-        case .right:
-          node.position = CGPoint(x: node.position.x + 10, y: node.position.y)
-        case .left:
-          node.position = CGPoint(x: node.position.x - 10, y: node.position.y)
-        case .downThenLeft, .downThenRight:
-          node.position = CGPoint(x: node.position.x, y: node.position.y - 10)
-        case .none:
-          break
+        // 1
+        if currentTime - timeOfLastMove < timePerMove {
+            return
         }
         
-        // 3
-
-      }
+        // 2
+        enumerateChildNodes(withName: InvaderType.name) { node, stop in
+            switch self.invaderMovementDirection {
+            case .right:
+                node.position = CGPoint(x: node.position.x + 10, y: node.position.y)
+            case .left:
+                node.position = CGPoint(x: node.position.x - 10, y: node.position.y)
+            case .downThenLeft, .downThenRight:
+                node.position = CGPoint(x: node.position.x, y: node.position.y - 10)
+            case .none:
+                break
+            }
+            
+            // 3
+            
+        }
         self.timeOfLastMove = currentTime
         self.determineInvaderMovementDirection()
         
     }
     
-    func determineInvaderMovementDirection() {
+    func processUserMotion(forUpdate currentTime: CFTimeInterval) {
       // 1
-      var proposedMovementDirection: InvaderMovementDirection = invaderMovementDirection
-      
-      // 2
-      enumerateChildNodes(withName: InvaderType.name) { node, stop in
+      if let ship = childNode(withName: kShipName) as? SKSpriteNode {
+        // 2
+        if let data = motionManager.accelerometerData {
+          // 3
+          if fabs(data.acceleration.x) > 0.2 {
+            // 4 How do you move the ship?
+            print("Acceleration: \(data.acceleration.x)")
+          }
+        }
+      }
+    }
+    
+    func determineInvaderMovementDirection() {
+        // 1
+        var proposedMovementDirection: InvaderMovementDirection = invaderMovementDirection
         
-        switch self.invaderMovementDirection {
-        case .right:
-          //3
-          if node.frame.maxX >= 200 {
-            proposedMovementDirection = .downThenLeft
+        // 2
+        enumerateChildNodes(withName: InvaderType.name) { node, stop in
             
-            stop.pointee = true
-          }
-        case .left:
-          //4
-          if node.frame.minX <= -200 {
-            proposedMovementDirection = .downThenRight
+            switch self.invaderMovementDirection {
+            case .right:
+                //3
+                if node.frame.maxX >= 200 {
+                    proposedMovementDirection = .downThenLeft
+                    
+                    stop.pointee = true
+                }
+            case .left:
+                //4
+                if node.frame.minX <= -200 {
+                    proposedMovementDirection = .downThenRight
+                    
+                    stop.pointee = true
+                }
+                
+            case .downThenLeft:
+                proposedMovementDirection = .left
+                
+                stop.pointee = true
+                
+            case .downThenRight:
+                proposedMovementDirection = .right
+                
+                stop.pointee = true
+                
+            default:
+                break
+            }
             
-            stop.pointee = true
-          }
-          
-        case .downThenLeft:
-          proposedMovementDirection = .left
-          
-          stop.pointee = true
-          
-        case .downThenRight:
-          proposedMovementDirection = .right
-          
-          stop.pointee = true
-          
-        default:
-          break
         }
         
-      }
-      
-      //7
-      if proposedMovementDirection != invaderMovementDirection {
-        invaderMovementDirection = proposedMovementDirection
-      }
+        //7
+        if proposedMovementDirection != invaderMovementDirection {
+            invaderMovementDirection = proposedMovementDirection
+        }
     }
     
     
     func makeInvader(ofType invaderType: InvaderType) -> SKNode {
-      // 1 This code needs to be changed so that each invader can be controlled seperatly, or at least so that each row of invaders can be controlled independatly of the group.
-      var invaderColor: SKColor
-      
-      switch(invaderType) {
-      case .a:
-        invaderColor = SKColor.red
-      case .b:
-        invaderColor = SKColor.green
-      case .c:
-        invaderColor = SKColor.blue
-      }
-      
-      // 2
-      let invader = SKSpriteNode(color: invaderColor, size: InvaderType.size)
-      invader.name = InvaderType.name
-      
-      return invader
+        // 1 This code needs to be changed so that each invader can be controlled seperatly, or at least so that each row of invaders can be controlled independatly of the group.
+        var invaderColor: SKColor
+        
+        switch(invaderType) {
+        case .a:
+            invaderColor = SKColor.red
+        case .b:
+            invaderColor = SKColor.green
+        case .c:
+            invaderColor = SKColor.blue
+        }
+        
+        // 2
+        let invader = SKSpriteNode(color: invaderColor, size: InvaderType.size)
+        invader.name = InvaderType.name
+        
+        return invader
     }
     
     func setupInvaders() {
-      // 1
-      let baseOrigin = CGPoint(x: -72, y: -24)
-      
-      for row in 0..<kInvaderRowCount {
-        // 2
-        var invaderType: InvaderType
+        // 1
+        let baseOrigin = CGPoint(x: -72, y: -24)
         
-        if row % 3 == 0 {
-          invaderType = .a
-        } else if row % 3 == 1 {
-          invaderType = .b
-        } else {
-          invaderType = .c
+        for row in 0..<kInvaderRowCount {
+            // 2
+            var invaderType: InvaderType
+            
+            if row % 3 == 0 {
+                invaderType = .a
+            } else if row % 3 == 1 {
+                invaderType = .b
+            } else {
+                invaderType = .c
+            }
+            
+            // 3
+            let invaderPositionY = CGFloat(row) * (InvaderType.size.height * 2) + baseOrigin.y
+            
+            var invaderPosition = CGPoint(x: baseOrigin.x, y: invaderPositionY)
+            
+            // 4
+            for _ in 1..<kInvaderColCount {
+                // 5
+                let invader = makeInvader(ofType: invaderType)
+                invader.position = invaderPosition
+                
+                addChild(invader)
+                
+                invaderPosition = CGPoint(
+                    x: invaderPosition.x + InvaderType.size.width + kInvaderGridSpacing.width,
+                    y: invaderPositionY
+                )
+            }
         }
-        
-        // 3
-        let invaderPositionY = CGFloat(row) * (InvaderType.size.height * 2) + baseOrigin.y
-        
-        var invaderPosition = CGPoint(x: baseOrigin.x, y: invaderPositionY)
-        
-        // 4
-        for _ in 1..<kInvaderColCount {
-          // 5
-          let invader = makeInvader(ofType: invaderType)
-          invader.position = invaderPosition
-          
-          addChild(invader)
-          
-          invaderPosition = CGPoint(
-            x: invaderPosition.x + InvaderType.size.width + kInvaderGridSpacing.width,
-            y: invaderPositionY
-          )
-        }
-      }
     }
     
     func setupShip() {
-      // 1
-      let ship = makeShip()
-      
-      // 2
-      ship.position = CGPoint(x: -25, y: -250)
-      addChild(ship)
+        // 1
+        let ship = makeShip()
+        
+        // 2
+        ship.position = CGPoint(x: -25, y: -250)
+        addChild(ship)
     }
-
+    
     func makeShip() -> SKNode {
-      let ship = SKSpriteNode(color: SKColor.green, size: kShipSize)
-      ship.name = kShipName
-      return ship
+        let ship = SKSpriteNode(color: SKColor.green, size: kShipSize)
+        ship.name = kShipName
+        return ship
     }
     
     func setupHud() {
-      // 1
-      let scoreLabel = SKLabelNode(fontNamed: "Courier")
-      scoreLabel.name = kScoreHudName
-      scoreLabel.fontSize = 25
-      
-      // 2
-      scoreLabel.fontColor = SKColor.green
-      scoreLabel.text = String(format: "Score: %04u", 0)
-      
-      // 3
+        // 1
+        let scoreLabel = SKLabelNode(fontNamed: "Courier")
+        scoreLabel.name = kScoreHudName
+        scoreLabel.fontSize = 25
+        
+        // 2
+        scoreLabel.fontColor = SKColor.green
+        scoreLabel.text = String(format: "Score: %04u", 0)
+        
+        // 3
         //check the other tutorials to see how those labels were added and positioned.
         scoreLabel.position = CGPoint(x:20, y: 250)
-      addChild(scoreLabel)
-      
-      // 4
-      let healthLabel = SKLabelNode(fontNamed: "Courier")
-      healthLabel.name = kHealthHudName
-      healthLabel.fontSize = 25
-      
-      // 5
-      healthLabel.fontColor = SKColor.red
-      healthLabel.text = String(format: "Health: %.1f%%", 100.0)
-      
-      // 6
+        addChild(scoreLabel)
+        
+        // 4
+        let healthLabel = SKLabelNode(fontNamed: "Courier")
+        healthLabel.name = kHealthHudName
+        healthLabel.fontSize = 25
+        
+        // 5
+        healthLabel.fontColor = SKColor.red
+        healthLabel.text = String(format: "Health: %.1f%%", 100.0)
+        
+        // 6
         //check the other tutorials to see how those labels were added and positioned.
-      healthLabel.position = CGPoint(x:20, y: 225)
-      
-      addChild(healthLabel)
+        healthLabel.position = CGPoint(x:20, y: 225)
+        
+        addChild(healthLabel)
     }
     
 }
